65b712946318eb3212ff78a69f56b7561ebb0963
José Alfredo Reyes de Antuñano <jardarubydv@gmail.com>
2026-04-07 03:51:37 -0600

    cambios directo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1436,12 +1436,19 @@ function generateMediaContent(panel) {
   if (panel.videoId) {
     html += `
       <h3 class="video-section-title">${featuredVideo}</h3>
+      
+      <div class="video-embed-wrapper">
+      <iframe src="https://www.youtube.com/embed/wpRwFqjHSiw?si=ktilEv4_cNysB8ba&amp;start=6568" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div> <br/>
+
       <div class="video-embed-wrapper">
         <iframe src="https://www.youtube.com/embed/g1_GukOgCZI" title="Video" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div> <br/>
       <div class="video-embed-wrapper">
         <iframe src="https://www.youtube.com/embed/${panel.videoId}" title="Video" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
+
+
     `;
   }
   
